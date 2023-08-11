bf63e4fe6d7d0e69497f681255a76ee323ca3b6b
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2023-08-12 02:09:16 +0800

    navlist i18n

--- components/NavList.vue
+++ components/NavList.vue
@@ -21,7 +21,7 @@ const props = defineProps({
   </CTransition> -->
   <div :class="open ? 'h-[248px] p-2' : 'h-0 p-0'" id="nav"
     class="flex flex-col items-center overflow-hidden gap-2 md:hidden absolute top-[96px] left-0 z-50 w-full bg-primary text-white  duration-200">
-    <NuxtLink v-for="(item, i) in list" :key="i" :to="item.src" class="link-btn w-full text-center">{{ item.name }}
+    <NuxtLink v-for="(item, i) in list" :key="i" :to="item.src" class="link-btn w-full text-center">{{ $t(item.name) }}
     </NuxtLink>
   </div>
 </template>
