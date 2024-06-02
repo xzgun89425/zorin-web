3342f87219479a1ece0533b9beb89d95c39d9cde
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2024-06-02 14:52:22 +0800

    fix:contact 調整

--- components/Footer.vue
+++ components/Footer.vue
@@ -50,22 +50,23 @@ function openQRcode() {
                 </div>
             </div>
             <div class="text-sm space-y-4 mt-4 md:mt-0 text-center md:text-end">
-                <a href="https://maps.app.goo.gl/shgTxo1HyEtxS5z1A"
-                    class="flex gap-4 items-center justify-center md:justify-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-white w-3 mt-1"
-                        viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                        <path
-                            d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                    </svg>
-                    {{ locale == 'zh-TW' ? locationText.zh : locationText.en }}
-                </a>
                 <div class="flex gap-4 items-center justify-center md:justify-end">
                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-white w-3 mt-1"
                         viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                         <path
                             d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                     </svg>
-                    ivan450870@gmail.com , eel012345@yahoo.com.tw
+                    {{ $t("taiwan") }}：
+                    eel012345@yahoo.com.tw
+                </div>
+                <div class="flex gap-4 items-center justify-center md:justify-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-white w-3 mt-1"
+                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path
+                            d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                    </svg>
+                    {{ $t("overseas") }}：
+                    ivan450870@gmail.com
                 </div>
             </div>
         </div>
--- pages/contactUs.vue
+++ pages/contactUs.vue
@@ -5,7 +5,7 @@
   <div class="flex flex-col w-full items-center min-h-[]">
     <Banner>{{ $t('header.contact') }}</Banner>
     <Container>
-      <ContainerDiv>
+      <ContainerDiv :title="'header.contact'">
         <div class="grid grid-cols-1 lg:grid-cols-4 gap-10 py-16 max-w-7xl w-full">
           <div class="col-span-1 flex flex-col items-center rounded-md p-6 relative">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-10 fill-primary"
@@ -22,8 +22,10 @@
               <path
                 d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
             </svg>
-            <h1 class="text-lg tracking-wider index-font mt-8">eel012345@yahoo.com.tw</h1>
-            <h1 class="text-lg tracking-wider index-font">ivan450870@gmail.com</h1>
+            <h1 class="text-base tracking-wider flex items-center index-font mt-8">{{ $t("taiwan")
+              }}：eel012345@yahoo.com.tw</h1>
+            <h1 class="text-base tracking-wider flex items-center index-font"> {{ $t("overseas") }}：ivan450870@gmail.com
+            </h1>
           </div>
           <div class="col-span-1 flex flex-col items-center rounded-md p-6 relative ">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-10 fill-primary"
@@ -31,9 +33,11 @@
               <path
                 d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
             </svg>
-            <a class="text-lg tracking-wider index-font mt-8">+886
-              968998665
-            </a>
+            <h1 class="text-base tracking-wider flex justify-start items-center index-font mt-8">{{ $t("taiwan")
+              }}：0919515222</h1>
+            <h1 class="text-base tracking-wider flex justify-start items-center index-font"> {{ $t("overseas")
+              }}：+886968998665
+            </h1>
           </div>
           <div class="col-span-1 flex flex-col items-center rounded-md p-6 relative">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-10 fill-primary"
@@ -41,16 +45,14 @@
               <path
                 d="M311 196.8v81.3c0 2.1-1.6 3.7-3.7 3.7h-13c-1.3 0-2.4-.7-3-1.5l-37.3-50.3v48.2c0 2.1-1.6 3.7-3.7 3.7h-13c-2.1 0-3.7-1.6-3.7-3.7V196.9c0-2.1 1.6-3.7 3.7-3.7h12.9c1.1 0 2.4 .6 3 1.6l37.3 50.3V196.9c0-2.1 1.6-3.7 3.7-3.7h13c2.1-.1 3.8 1.6 3.8 3.5zm-93.7-3.7h-13c-2.1 0-3.7 1.6-3.7 3.7v81.3c0 2.1 1.6 3.7 3.7 3.7h13c2.1 0 3.7-1.6 3.7-3.7V196.8c0-1.9-1.6-3.7-3.7-3.7zm-31.4 68.1H150.3V196.8c0-2.1-1.6-3.7-3.7-3.7h-13c-2.1 0-3.7 1.6-3.7 3.7v81.3c0 1 .3 1.8 1 2.5c.7 .6 1.5 1 2.5 1h52.2c2.1 0 3.7-1.6 3.7-3.7v-13c0-1.9-1.6-3.7-3.5-3.7zm193.7-68.1H327.3c-1.9 0-3.7 1.6-3.7 3.7v81.3c0 1.9 1.6 3.7 3.7 3.7h52.2c2.1 0 3.7-1.6 3.7-3.7V265c0-2.1-1.6-3.7-3.7-3.7H344V247.7h35.5c2.1 0 3.7-1.6 3.7-3.7V230.9c0-2.1-1.6-3.7-3.7-3.7H344V213.5h35.5c2.1 0 3.7-1.6 3.7-3.7v-13c-.1-1.9-1.7-3.7-3.7-3.7zM512 93.4V419.4c-.1 51.2-42.1 92.7-93.4 92.6H92.6C41.4 511.9-.1 469.8 0 418.6V92.6C.1 41.4 42.2-.1 93.4 0H419.4c51.2 .1 92.7 42.1 92.6 93.4zM441.6 233.5c0-83.4-83.7-151.3-186.4-151.3s-186.4 67.9-186.4 151.3c0 74.7 66.3 137.4 155.9 149.3c21.8 4.7 19.3 12.7 14.4 42.1c-.8 4.7-3.8 18.4 16.1 10.1s107.3-63.2 146.5-108.2c27-29.7 39.9-59.8 39.9-93.1z" />
             </svg>
+            <h1 class="text-base tracking-wider flex justify-start items-center index-font mt-8">{{ $t("taiwan")
+              }}：zorinaudio</h1>
             <a href="https://line.me/ti/p/mmn3F3EX4U"
-              class="text-lg tracking-wider index-font mt-8 hover:cursor-pointer hover:text-blue-500 duration-200">+886
-              968998665
+              class="text-base tracking-wider index-font hover:cursor-pointer hover:text-blue-500 duration-200">{{
+                $t("overseas")
+              }}：0968998665
             </a>
           </div>
-        </div>
-        <div class="w-full aspect-w-16 aspect-h-6">
-          <iframe class="w-full"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d909.8636201410113!2d120.516359075874!3d24.190857404868684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346940052f213f41%3A0x4513a63fd23a4603!2zNDM05Y-w5Lit5biC6b6N5LqV5Y2A6IyE5oqV6Lev56aP6aCt5bSZ5be3MeiZnw!5e0!3m2!1szh-TW!2stw!4v1716888214954!5m2!1szh-TW!2stw"
-            style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </ContainerDiv>
     </Container>
