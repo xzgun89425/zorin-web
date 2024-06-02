8deda911fb1cf9e1c080811c076a68beb965dae8
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2024-06-02 14:55:04 +0800

    fix:i18n contact

--- components/Header.vue
+++ components/Header.vue
@@ -1,4 +1,9 @@
 <script setup>
+const { locale } = useI18n();
+const emailText = reactive({
+  zh: 'eel012345@yahoo.com.tw',
+  en: 'ivan450870@gmail.com'
+});
 const open = ref(false);
 const list = reactive([
   { name: "header.home", src: "/" },
@@ -38,7 +43,7 @@ function openQRcode() {
         <svg xmlns="http://www.w3.org/2000/svg" class="w-3 mt-1" viewBox="0 0 512 512">
           <path
             d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
-        </svg>ivan450870@gmail.com
+        </svg>{{locale == 'zh-TW' ? emailText.zh : emailText.en}}
       </div>
       <div class="flex items-center gap-3">
         <a href="https://www.facebook.com/ZorinAudio"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 mt-1"
